d758ff3ca02aa2c9627ad25fe7913f710b8673a9
U M MANOJ KUMAR <51526294+ummanojkumar@users.noreply.github.com>
2022-02-02 23:24:55 +0530

    Revert "if is added"

--- Loacal_Project/src/com/demo/FirstClass.java
+++ Loacal_Project/src/com/demo/FirstClass.java
@@ -6,18 +6,9 @@ public class FirstClass {
 		
 		System.out.println("welcome to Hindupur");
 		
-		int x=21;
-		if (x>=20)
-		{
-			System.out.println("larger number");
-		}
-		else
-		{
-			System.out.println("smaller number");
-		}
 		
-		// comment is added
-		System.out.println("hii manoj");
+		
+		
 
 	}
 
