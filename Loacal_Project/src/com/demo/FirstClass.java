aa9715b783c4787051c398ddf8c945e8c63931bc
ummanojkumar123 <98905181+ummanojkumar123@users.noreply.github.com>
2022-02-02 23:08:45 +0530

    delete some code
    code is deleted sucessfully

--- Loacal_Project/src/com/demo/FirstClass.java
+++ Loacal_Project/src/com/demo/FirstClass.java
@@ -6,33 +6,7 @@ public class FirstClass {
 		
 		System.out.println("welcome to Hindupur");
 		
-		System.out.println("Hi Mr Manoj");
 		
-		int x=21;
-		if (x>=20)
-		{
-			System.out.println("larger number");
-		}
-		else
-		{
-			System.out.println("smaller number");
-		}
-		
-		// comment is added
-		System.out.println("hii manoj");
-		
-		int x=21;
-		if (x>=20)
-		{
-			System.out.println("larger number");
-		}
-		else
-		{
-			System.out.println("smaller number");
-		}
-		
-		// comment is added
-		System.out.println("hii manoj");
 		
 		
 
