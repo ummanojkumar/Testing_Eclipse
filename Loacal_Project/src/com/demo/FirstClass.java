8cbbedbb029b3af18aa95e19d10e13778799e2da
ummanojkumar123 <98905181+ummanojkumar123@users.noreply.github.com>
2022-02-02 23:12:31 +0530

    if is added

--- Loacal_Project/src/com/demo/FirstClass.java
+++ Loacal_Project/src/com/demo/FirstClass.java
@@ -6,9 +6,18 @@ public class FirstClass {
 		
 		System.out.println("welcome to Hindupur");
 		
+		int x=21;
+		if (x>=20)
+		{
+			System.out.println("larger number");
+		}
+		else
+		{
+			System.out.println("smaller number");
+		}
 		
-		
-		
+		// comment is added
+		System.out.println("hii manoj");
 
 	}
 
