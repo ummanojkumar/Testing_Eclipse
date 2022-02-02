290fd759ff113a0a97061f72d2970d38a14f88cb
UM20279977 <UM20279977@APL-9SXMS93>
2022-02-02 19:33:17 +0530

    third commit from wipro

--- Loacal_Project/src/com/demo/FirstClass.java
+++ Loacal_Project/src/com/demo/FirstClass.java
@@ -3,7 +3,8 @@ package com.demo;
 public class FirstClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Hii Manoj");
 
 	}
 
---
+++ Loacal_Project/src/com/demo/SecondClass.java
@@ -0,0 +1,9 @@
+package com.demo;
+
+public class SecondClass 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("this is my second class");
+	}
+}
